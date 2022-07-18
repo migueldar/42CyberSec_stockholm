0177c347147e313b506ca0e062b2b2f06bfec67c
Migueldar <mdearpe@ucm.es>
2022-07-18 21:25:18 +0200

    added linux compatibility

--- builder.sh
+++ builder.sh
@@ -1,2 +1,6 @@
 docker build -t $1 .
-docker run --name $2 -it -v $(PWD)/code:/code -v $(PWD)/infection:/root/infection $1
+if [ $(uname) = "Darwin" ]; then
+	docker run --name $2 -it -v $PWD/code:/code -v $PWD/infection:/root/infection $1
+elif [ $(uname) = "Linux" ]; then
+	docker run --name $2 -it -v $PWD/code:/code -v $PWD/infection:/root/infection --add-host host.docker.internal:host-gateway $1
+fi
